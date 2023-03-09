c69606af04d1991e3151d01361ca409411fb9151
latte <something42343@test.so>
2023-03-09 13:24:41 +0000

    bar graph styles

--- src/components/view/home/view_components/ProductViews.js
+++ src/components/view/home/view_components/ProductViews.js
@@ -9,6 +9,44 @@ const ProductViews = ({filter: Filter}) => {
                 </div>
                 <Filter />
             </div> 
+            <div className="graph">
+                <div className="graph_main-view">
+                    <div className="graph_bars-container">
+                        <div className="bar-container">
+                        </div>
+                        <div className="bar-container">
+                        </div>
+                        <div className="bar-container">
+                        </div>
+                        <div className="bar-container">
+                        </div>
+                        <div className="bar-container">
+                        </div>
+                        <div className="bar-container">
+                        </div>
+                        <div className="bar-container">
+                        </div>
+                    </div>
+                    <div className="graph-x-axis">
+                        <span> 22 </span>
+                        <span> 23 </span>
+                        <span> 24 </span>
+                        <span> 25 </span>
+                        <span> 26 </span>
+                        <span> 27 </span>
+                        <span> 28 </span>
+                    </div>
+                </div>
+                <div className="graph-y-axis">
+                        <span> 35 </span>
+                        <span> 30 </span>
+                        <span> 25 </span>
+                        <span> 20 </span>
+                        <span> 15 </span>
+                        <span> 10 </span>
+                        <span> 5 </span>
+                    </div>
+            </div>
         </div>
     )
 }
